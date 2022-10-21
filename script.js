88109d2d46c5bbf94b15958dc99bd7c30a573159
Eren Korkmaz <erenkorkmaz88@hotmail.com>
2022-10-21 11:58:32 +0300

    First

---
+++ script.js
@@ -0,0 +1,35 @@
+
+
+function setWithExpiry(key, value, ttl) {
+    const now = new Date()
+
+    const item = {
+        value: value,
+        expiry: now.getTime() + ttl,
+    }
+    localStorage.setItem(key, JSON.stringify(item))
+}
+
+function getWithExpiry(key) {
+    const itemStr = localStorage.getItem(key)
+
+    if (!itemStr) {
+        return null
+    }
+
+    const item = JSON.parse(itemStr)
+    const now = new Date()
+
+    if (now.getTime() > item.expiry) {
+        localStorage.removeItem(key)
+        return null
+    }
+    return item.value
+}
+
+
+setWithExpiry("key", "value", 5000)
+
+console.log(getWithExpiry("key"))
+
+setTimeout(function(){ console.log(getWithExpiry("key"))}, 6000);
